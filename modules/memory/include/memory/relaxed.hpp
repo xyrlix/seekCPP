fa5c95b121ddb090462a9735b2ca422d2bf1eef0
xyrlix@qq.com <xyrlix@outlook.com>
2026-07-04 19:19:43 +0800

    chore: add clang-format config and update memory module

--- modules/memory/include/memory/relaxed.hpp
+++ modules/memory/include/memory/relaxed.hpp
@@ -6,6 +6,7 @@
 
 namespace memory {
 
+// 纯计数器，不使用锁，不使用原子操作
 class RequestCounter {
 private:
     std::atomic<uint32_t> total_{0};
@@ -18,6 +19,15 @@ public:
         return total_.load(std::memory_order_relaxed);
     }
 };
+
+class IdGenerator {
+private:
+    std::atomic<uint32_t> id_{0};
+public:
+    uint32_t next() {
+        return id_.fetch_add(1, std::memory_order_relaxed) + 1;
+    }
+}
 
 }
 
